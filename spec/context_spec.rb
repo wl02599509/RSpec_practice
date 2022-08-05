dd443c952d2559f08e9e600c2ddc6bbd127fca1e
Mike <wl02599509@gmail.com>
2022-08-05 22:23:55 +0800

    context 組織語意，使用 with when without

---
+++ spec/context_spec.rb
@@ -0,0 +1,32 @@
+class Member 
+  attr_accessor :rank
+  def initialize(rank)
+    @rank = rank
+  end
+
+  def bonus
+    if @rank === 'silver'
+      0.9
+    elsif @rank === 'golden'
+      0.8
+    end
+  end
+end
+
+RSpec.describe ".bonus method" do
+  let(:member) { Member.new('normal') }
+
+  context "member is silver rank" do
+    it 'should feedback 10% discount.' do
+      member.rank = 'silver'
+      expect(member.bonus).to eq(0.9)
+    end
+  end
+
+  context "member is golden rank" do
+    it 'should feedback 20% discount.' do
+      member.rank = 'golden'
+      expect(member.bonus).to eq(0.8)
+    end
+  end
+end
