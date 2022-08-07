45e307c7b63cce1dc89c2bc276434d73453027aa
Mike <wl02599509@gmail.com>
2022-08-08 00:07:47 +0800

    subject and is_expect method

--- spec/subject_spec.rb
+++ spec/subject_spec.rb
@@ -2,6 +2,9 @@ RSpec.describe String do
   subject {"ABCDE"}
 
   it "should be 5 words" do
-    expect(subject.length).to eq ("AVC")
+    expect(subject.length).to eq (5)
   end
+
+  it { is_expected.to eq("ABCDE") }
+    
 end
